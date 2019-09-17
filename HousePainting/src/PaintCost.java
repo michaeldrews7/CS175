63075d5a63107e75a78b27143f99e2368eb45956
Michael Drews <s1235663@monmouth.edu>
2019-09-16 20:56:53 -0400

    variables created

---
+++ HousePainting/src/PaintCost.java
@@ -0,0 +1,21 @@
+
+public class PaintCost {
+
+	public static void main(String[] args) {
+		/*
+		 * This program will calculate the cost of painting a house
+		 */
+		double houseLengthFeet;
+		double houseWidthFeet;
+		double houseHeightFeet;
+		double windowLengthFeet;
+		double windowWidthFeet;
+		int windowNumber;
+		double doorLengthFeet;
+		double doorWidthFeet;
+		int doorNumber;
+		
+		
+	}
+
+}

ff70c8cec784d423dcde92964a8e769b49a06090
Michael Drews <s1235663@monmouth.edu>
2019-09-18 20:56:25 -0400

    Updates for the house painting code

--- HousePainting/src/PaintCost.java
+++ HousePainting/src/PaintCost.java
@@ -1,19 +1,47 @@
+import java.util.Scanner;
 
 public class PaintCost {
+
+	
+	
+	
 
 	public static void main(String[] args) {
 		/*
 		 * This program will calculate the cost of painting a house
 		 */
-		double houseLengthFeet;
-		double houseWidthFeet;
-		double houseHeightFeet;
-		double windowLengthFeet;
-		double windowWidthFeet;
+		//double houseLengthFeet;
+		int houseWidthFeet;
+		int houseWidthInches;
+		int houseHeightFeet;
+		int houseHeightInches;
+		int houseLengthFeet;
+		int houseLengthInches;
+		int windowWidthFeet;
+		int windowWidthInches;
+		int windowLengthFeet;
+		int windowLengthInches;
 		int windowNumber;
-		double doorLengthFeet;
-		double doorWidthFeet;
+		int doorLengthFeet;
+		int doorLengthInches;
+		int doorWidthFeet;
+		int doorWidthInches;
 		int doorNumber;
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Please enter the length of the house in whole feet (you will be asked inches next): ");
+		houseLengthFeet = in.nextInt();
+		System.out.print("Please enter the remainding number of inches for the length of the house: ");
+		houseLengthInches = in.nextInt();
+		System.out.print("Please enter the width of the house in whole feet (you will be asked inches next): ");
+		houseWidthFeet = in.nextInt();
+		System.out.print("Please enter the remainding number of inches for the width of the house: ");
+		houseWidthInches = in.nextInt();
+		System.out.print("Please enter the height of the house in whole feet (you will be asked inches next): ");
+		houseHeightFeet = in.nextInt();
+		System.out.print("Please enter the remainding number of inches for the height of the house: ");
+		houseHeightInches = in.nextInt();
+		
 		
 		
 	}

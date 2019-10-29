7fd507be563ec1e61df46e52b9fb54d8a97928a4
Michael Drews <s1235663@monmouth.edu>
2019-10-28 20:28:16 -0400

    finished groceryv2

---
+++ GroceryDiscount/src/groceryDiscountv2.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class groceryDiscountv2 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		
+		System.out.print("Please enter the cost of your groceries or 'e' to end: ");
+		while (in.hasNextDouble()) {
+			double moneySpent = in.nextDouble();
+		
+			double moneySpent8 = moneySpent * .08;
+			double moneySpent10 = moneySpent * .1;
+			double moneySpent12 = moneySpent * .12;
+			double moneySpent14 = moneySpent * .14;
+		
+		
+			if (moneySpent < 10) {
+				System.out.println("You are not eligible for a coupon at this time.");
+			}
+			else if (moneySpent < 60.01) {
+				System.out.print("You win a discount coupon of $");
+				System.out.printf("%.2f", moneySpent8);
+				System.out.println(". (8% of your purchase)");
+			}
+			else if (moneySpent < 150.01) {
+				System.out.print("You win a discount coupon of $");
+				System.out.printf("%.2f", moneySpent10);
+				System.out.println(". (10% of your purchase)");
+			}
+			else if (moneySpent < 210.01) {
+				System.out.print("You win a discount coupon of $");
+				System.out.printf("%.2f", moneySpent12);
+				System.out.println(". (12% of your purchase)");
+			}
+			else {
+				System.out.print("You win a discount coupon of $");
+				System.out.printf("%.2f", moneySpent14);
+				System.out.println(". (14% of your purchase)");
+			}
+			if (moneySpent == 0)
+				break;
+			
+			System.out.print("Please enter the cost of your groceries or 'e' to end: ");
+		}
+		
+	}
+
+}

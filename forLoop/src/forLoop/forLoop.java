b86d57c6ca58db96924d81f31db06f6bb24487c8
Michael Drews <s1235663@monmouth.edu>
2019-11-11 20:26:50 -0500

    multiplication table

---
+++ forLoop/src/forLoop/forLoop.java
@@ -0,0 +1,15 @@
+package forLoop;
+
+public class forLoop {
+
+	public static void main(String[] args) {
+	    for (int a = 1; a <= 12; a++) {
+	        for (int b = 1; b <= 12; b++) {
+	            System.out.printf("%4d", (a*b));
+	        }
+	        System.out.println();
+	    }
+
+	}
+
+}

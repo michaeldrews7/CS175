1330aaa21fc5b55a77998f7898e01b0681a6868b
Michael Drews <s1235663@monmouth.edu>
2019-11-13 20:02:12 -0500

    version 1.1

--- forLoop/src/forLoop/forLoop.java
+++ forLoop/src/forLoop/forLoop.java
@@ -3,9 +3,19 @@ package forLoop;
 public class forLoop {
 
 	public static void main(String[] args) {
+		
+		for (int c = 0; c <= 12; c++) {
+			System.out.printf("%5d", c);
+		}
+		System.out.println(" ");
+		System.out.println("_________________________________________________________________");
+		//System.out.println(" ");
+		
 	    for (int a = 1; a <= 12; a++) {
+	    	System.out.println();
+	    	System.out.printf("%5s", a + " | ");
 	        for (int b = 1; b <= 12; b++) {
-	            System.out.printf("%4d", (a*b));
+	            System.out.printf("%5d", (a*b));
 	        }
 	        System.out.println();
 	    }

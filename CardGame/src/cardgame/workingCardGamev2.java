e7cdeff7666b47e009c647586cb64fbbb9ffee7f
Michael Drews <s1235663@monmouth.edu>
2019-12-09 20:10:44 -0500

    updated v2

--- CardGame/src/cardgame/workingCardGamev2.java
+++ CardGame/src/cardgame/workingCardGamev2.java
@@ -10,58 +10,78 @@ public class workingCardGamev2 {
 
 		Random generator = new Random();
 
-		int countH = 0;
-		int countD = 0;
-		int countC = 0;
-		int countS = 0;
-		int countDraw = 0;
-		int myDraw = 0;
-		int yourDraw = 0;
+		int countH = 0, countD = 0, countC = 0, countS = 0, countDraw = 0, myDraw = 0, yourDraw = 0;
 		String mySuit = null;
 		String yourSuit = null;
 		int[] myHand = new int[5];
 		int[] yourHand = new int[5];
 		int[] deck = new int[52];
 		boolean winner = false;
-		int myMax=0;
-		int myMin=0;
-		int yourMax=0;
-		int yourMin=0;
+		int mySuitHigh=0;
+		int mySuitLow=0;
+		int yourSuitHigh=0;
+		int yourSuitLow=0;
 		int yourGoodCount=0;
 		int myGoodCount=0;
+		int card =0;
+		boolean goodCard=false;
+		
 		//take all cards out of play to begin game
-		//for (int c= 0;c <= 52;c++)
-			//{deck[c]=0;}
-		//check
-
-		//assigns card values to both hands and determines suit
+		for (int c=0;c<52;c++)
+		{deck[c]=0;}
+		
+		//assigns card values to MYHAND and determines suit
 		for (int i = 0; i < myHand.length; i++) {
-			//int card = generator.nextInt(52)+1;
-			//if deck[card]==0
-					
-			myHand[i] = generator.nextInt(52) + 1;
-			//check
-			if (myHand[i] >= 1 && myHand[i] <= 13)
+
+
+			//draw a card until a card if drawn that is not in play, insert into hand, mark as out of play
+			while (goodCard==false) 
+			{		
+				card = generator.nextInt(52)+1;
+				if (deck[card-1]==0) 
+					{myHand[i] = card; deck[card-1]=1;break;}
+			}	
+			//end of while loop	
+
+			if (myHand[i] >= 1 && myHand[i] <= 13) {
 				countH++;
-			else if (myHand[i] >= 14 && myHand[i] <= 26)
+			}
+			else if (myHand[i] >= 14 && myHand[i] <= 26) {
 				countD++;
-			else if (myHand[i] >= 27 && myHand[i] <= 39)
+			}
+			else if (myHand[i] >= 27 && myHand[i] <= 39) {
 				countC++;
-			else if (myHand[i] >= 40 && myHand[i] <= 52)
+			}
+			else if (myHand[i] >= 40 && myHand[i] <= 52) {
 				countS++;
-			if (countH >= countD && countH >= countC && countH >= countS)
+			}
+			if (countH >= countD && countH >= countC && countH >= countS) {
 				mySuit = "Hearts";
-			else if (countD >= countH && countD >= countC && countD >= countS)
+			}
+			else if (countD >= countH && countD >= countC && countD >= countS) {
 				mySuit = "Diamonds";
-			else if (countC >= countH && countC >= countD && countC >= countS)
+			}
+			else if (countC >= countH && countC >= countD && countC >= countS) {
 				mySuit = "Clubs";
-			else if (countS >= countH && countS >= countC && countS >= countD)
+			}
+			else if (countS >= countH && countS >= countC && countS >= countD) {
 				mySuit = "Spades";
+			}
 		}
+
+		//assigns card values to YOURHAND and determines suit
 		for (int i = 0; i < yourHand.length; i++) {
-			yourHand[i] = generator.nextInt(52) + 1;
-			//check
-			if (yourHand[i] >= 1 && yourHand[i] <= 13) {
+
+			//draw a card until a card if drawn that is not in play, insert into hand, mark as out of play
+			while (goodCard==false) 
+			{		
+				card = generator.nextInt(52)+1;
+				if (deck[card-1]==0) 
+					{yourHand[i] = card; deck[card-1]=1;break;}
+			}	
+			//end of while loop	
+
+				if (yourHand[i] >= 1 && yourHand[i] <= 13) {
 				countH++;
 			}
 			else if (yourHand[i] >= 14 && yourHand[i] <= 26) {
@@ -90,110 +110,111 @@ public class workingCardGamev2 {
 		System.out.println("Your hand has five cards: " + Arrays.toString(yourHand));
 		System.out.println("My suit is " + mySuit + ".");
 		System.out.println("Your suit is " + yourSuit + ".");
-		if (mySuit.equals("Hearts")) {
-			myMin=1;
-			myMax = 13;
-		}
-		if (mySuit.equals("Diamonds")) {
-			myMin=14;
-			myMax = 26;
-		}
-		if (mySuit.equals("Clubs")) {
-			myMin=27;
-			myMax = 39;
-		}
-		if (mySuit.equals("Spades")) {
-			myMin=40;
-			myMax = 52;
-		}
-		if (yourSuit.equals("Hearts")) {
-			yourMin=0;
-			yourMax = 13;
-		}
-		if (yourSuit.equals("Diamonds")){
-			yourMin=14;
-			yourMax= 26;
-		}
-		if (yourSuit.equals("Clubs")){
-			yourMin=27;
-			yourMax= 39;
-		}
-		if (yourSuit.equals("Spades")){
-			yourMin=40;
-			yourMax= 52;
-		}
 
-		//repeats card draw until winner
+		//PRINT THE DECK ARRAY 1 MEANS CARD IS IN A HAND 0 MEANS IT IS AVAILABLE 
+		System.out.println("The deck: " + Arrays.toString(deck));
+		System.out.println();	
+
+		if (mySuit.equals("Hearts")){mySuitLow=1;mySuitHigh = 13;}
+		if (mySuit.equals("Diamonds")){mySuitLow=14;mySuitHigh = 26;}
+		if (mySuit.equals("Clubs")){mySuitLow=27;mySuitHigh = 39;}
+		if (mySuit.equals("Spades")){mySuitLow=40;mySuitHigh = 52;}
+		if (yourSuit.equals("Hearts")){yourSuitLow=0;yourSuitHigh = 13;}
+		if (yourSuit.equals("Diamonds")){yourSuitLow=14;yourSuitHigh = 26;}
+		if (yourSuit.equals("Clubs")){yourSuitLow=27;yourSuitHigh = 39;}
+		if (yourSuit.equals("Spades")){yourSuitLow=40;yourSuitHigh = 52;}
+
+		//THE GAMELOOP repeats card draw until winner
 		while (!winner) 
 		{
 			countDraw++;
-			myDraw = generator.nextInt(52) + 1;
-			//check
+			//draw a card for MYHAND until a card if drawn that is not in play
+			while (goodCard==false) 
+			{		
+				card = generator.nextInt(52)+1;
+				if (deck[card-1]==0) 
+					{myDraw=card;break;}
+			}	
+			//end of while loop	
 			System.out.println("My draw " + countDraw + ": " + myDraw);
-			yourDraw = generator.nextInt(52) + 1;
-			//check
+
+			//draw a card for YOURHAND until a card if drawn that is not in play
+			while (goodCard==false) 
+			{		
+				card = generator.nextInt(52)+1;
+				if (deck[card-1]==0) 
+					{yourDraw=card;break;}
+			}	
+			//end of while loop	
 			System.out.println("Your draw " + countDraw + ": " + yourDraw);
 
-			//This code for myHand
-			if (myDraw>=myMin && myDraw<=myMax) 
+			//Insert a card into MYHAND if it is in mysuit 
+			if (myDraw>=mySuitLow && myDraw<=mySuitHigh) 
 			{
 				for (int i = 0; i < 5; i++) 
 				{
-					if (myHand[i] < myMin || myHand[i]> myMax) {
-						deck[myHand[i]]=0;
+					if (myHand[i] < mySuitLow || myHand[i]> mySuitHigh) 
+					{
+						deck[myHand[i]-1]=0;
 						myHand[i] = myDraw;
-						break;
-					}
+						deck[myDraw-1]=1;
+						break;}
 				}
 						
 						
 			}
 			myGoodCount = 0;
+
+			//check if MYHAND is a winner
 			for (int j=0; j<5;j++) 
 			{
-				if (myHand[j]>=myMin && myHand[j]<=myMax)
+				if (myHand[j]>=mySuitLow && myHand[j]<=mySuitHigh)
 				{
 					myGoodCount = myGoodCount+1;
 				}
 			}
 			System.out.println("\tMy new hand is " + Arrays.toString(myHand));
-			if (myGoodCount == 5 ){
-				winner=true;
-				System.out.println("I am the winner.");
-				break;
-			}
+			if (myGoodCount == 5 )
+					{winner=true;System.out.println("I am the winner.");break;}
 			
-			//This code for yourHand
-			if (yourDraw>=yourMin && yourDraw<=yourMax) 
+			//Insert a card into YOURHAND if it is in yoursuit 
+			if (yourDraw>=yourSuitLow && yourDraw<=yourSuitHigh) 
 			{
 				for (int i = 0; i < 5; i++) 
 				{
-					if (yourHand[i] < yourMin || yourHand[i]> yourMax) {
+					if (yourHand[i] < yourSuitLow || yourHand[i]> yourSuitHigh) 
+					{
+						deck[yourHand[i]-1]=0;
 						yourHand[i] = yourDraw;
-						break;
-					}
+						deck[yourDraw-1]=0;
+					break;}
 				}
 						
 						
 			}
 			yourGoodCount = 0;
+
+			//check if YOURHAND a winner
 			for (int j=0; j<5;j++) 
 			{
-				if (yourHand[j]>=yourMin && yourHand[j]<=yourMax)
+				if (yourHand[j]>=yourSuitLow && yourHand[j]<=yourSuitHigh)
 				{
 					yourGoodCount = yourGoodCount+1;
 				}
 			}
 			System.out.println("\tYour new hand is " + Arrays.toString(yourHand));
-			if (yourGoodCount == 5 ){
-				winner=true;
-				System.out.println("You are the winner.");
-				break;
-			}
+			if (yourGoodCount == 5 )
+					{winner=true;System.out.println("You are the winner.");break;}
+
+			//PRINT THE DECK ARRAY 1 MEANS CARD IS IN A HAND 0 MEANS IT IS AVAILABLE 
+			System.out.println("The deck: " + Arrays.toString(deck));
+			System.out.println();	
 			
 		}		
 		System.out.println("The game is over.");
 
+		//PRINT THE DECK ARRAY 1 MEANS CARD IS IN A HAND 0 MEANS IT IS AVAILABLE 
+		System.out.println("The deck: " + Arrays.toString(deck));
 
 	}
 
